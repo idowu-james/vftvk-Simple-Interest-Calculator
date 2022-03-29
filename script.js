f2f336c43fda36876c33c87c645dd06aa2fcb2e2
James Idowu <idowu_james@rocketmail.com>
2022-03-29 17:42:39 +0000

    final project submition

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ function compute()
     var years = parseInt(document.getElementById("years").value);
 
     var interest = principal * years * rate / 100;
+    var totalAmount = interest + principal;
     var year = new Date().getFullYear()+parseInt(years);
 
     
 
-    if (principal == 0 || principal < 0) {
+    if (document.getElementById("principal").value.length == 0 || principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
     }
 
     else{
-        document.getElementById("result").innerHTML="<b>If</b> you deposit: <mark>"+principal+"</mark>,\<br\><b>At</b> an interest rate of: <mark>"+rate+"%</mark>\<br\><b>You</b> will receive an Interest of: <mark>"+interest+"</mark>,\<br\><b>In</b> the year: <mark>"+year+"</mark>\<br\>"   
+        document.getElementById("result").innerHTML="<b>If</b> you deposit: <mark>"+principal+"</mark>,\<br\><b>At</b> an interest rate of: <mark>"+rate+"%</mark>\<br\><b>You</b> will receive an Interest of: <mark>"+interest+"</mark>,\<br\><b>In</b> the year: <mark>"+year+"</mark>\<br\>\<hr\><b>Extra</b>\<br\><b>Total Amount</b> after "+years+" years is: <mark>"+totalAmount+"</mark>\<br\>"   
     }
 
      
@@ -26,6 +27,6 @@ function compute()
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
 
